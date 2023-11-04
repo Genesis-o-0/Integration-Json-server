f758a719d214504e45c568ca57a9318573d8e735
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-11-04 18:15:51 +0200

    Add integration with Json server

--- src/Todolist/list.jsx
+++ src/Todolist/list.jsx
@@ -1,40 +1,78 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../config/axios.config.js";
 const Todolist = () => {
   const [todos, setTodos] = useState([]);
+  const [tasktitle, setTaskTitle] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    setTaskTitle((s) => e.target.value);
+  };
+  const getTodos = async (search) => {
+    const response = await axiosInstance.get("/todos", {
+      params: { q: search },
+    });
+    setTodos(response.data);
+  };
 
-  const handleDelete = (id) => {};
-  const handleEdit = (content) => {};
-  const handleDone = (status) => {};
+  const handleDelete = (id) => {
+    axiosInstance.delete(`/todos/${id}`).then(() => getTodos());
+  };
+
+  const handleSearch = (e) => {
+    setSearchValue((s) => e.target.value);
+  };
+  const handleEdit = ({ title, completed, id }) => {
+    axiosInstance
+      .patch(`/todos/${id}`, { completed: false })
+      .then(() => getTodos());
+  };
+  const handleDone = ({ title, completed, id }) => {
+    axiosInstance
+      .patch(`/todos/${id}`, { completed: true })
+      .then(() => getTodos());
+  };
 
   const addTask = async (e) => {
     e.preventDefault();
+    axiosInstance
+      .post("/todos", {
+        title: tasktitle,
+        completed: false,
+      })
+      .then((data) => getTodos());
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => getTodos(searchValue), 3000);
+    return () => clearTimeout(timer);
+  }, [searchValue]);
 
   return (
     <div className="todolist">
       <div className="search" onSubmit={addTask}>
-        <input type="text" placeholder="Search ex: todo 1" />
+        <input
+          type="text"
+          placeholder="Search ex: todo 1"
+          onChange={(e) => handleSearch(e)}
+          value={searchValue}
+        />
       </div>
       <form className="addTask" onSubmit={addTask}>
         <input
           type="text"
           onChange={handleChange}
+          value={tasktitle}
           placeholder="Add a task........"
         />
         <button className="addtask-btn">Add Task</button>
       </form>
       <div className="lists">
         {todos?.map((todo, id) => (
-          <div
-            key={id}
-            className={`list ${todo.isCompleted ? "completed" : ""}`}
-          >
-            <p> {todo.taskName}</p>
+          <div key={id} className={`list ${todo.completed ? "completed" : ""}`}>
+            <p> {todo.title}</p>
             <div className="span-btns">
-              {!todo.isCompleted && (
+              {!todo.completed && (
                 <span onClick={() => handleDone(todo)} title="completed">
                   ✓
                 </span>
